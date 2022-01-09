abfce76bdc14344fce1b6b61af484c02999b9db8
mkolyasev <97382431+mkolyasev@users.noreply.github.com>
2022-01-09 15:22:09 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication12/ConsoleApplication12.cpp
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    printf("Input the array elements:\n");
+    int a[1000], i = 0, n = 0, c, min, k;
+
+    do
+    {
+        scanf_s("%d", &a[n++]);
+    } while ((getchar()) != '\n');
+
+    for (int i = 0; i < n; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < n; j++)
+            if (a[min] > a[j])
+                min = j;
+        k = a[min];
+        a[min] = a[i];
+        a[i] = k;
+    }
+
+    printf("Sorted array:\n");
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
